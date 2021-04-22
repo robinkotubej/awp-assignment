cb1dc1816322a3647b664ea111ba894f9fbbdd94
Robin Kotubej <robinkotubej@Robins-MacBook-Pro.local>
2021-04-22 18:02:11 +0200

    test actions

--- awp-assignment-frontend/src/constants.tsx
+++ awp-assignment-frontend/src/constants.tsx
@@ -1,22 +1,21 @@
 export const COLOR = {
-  GREEN: '#789045',
-  RED: '#E53333',
-  ORANGE: '#C38147',
-  YELLOW: '#E0C34F',
-  GOOGLE_RED: '#c6533e',
-  FACEBOOK_BLUE: '#5275a1',
+  GREEN: "#789045",
+  RED: "#E53333",
+  ORANGE: "#C38147",
+  YELLOW: "#E0C34F",
+  GOOGLE_RED: "#c6533e",
 
   HOVER: {
-    GREEN: '#829E47',
-    GOOGLE_RED: '#E25439',
-    FACEBOOK_BLUE: '#537EB5',
-    RED: '#FF4D4D',
+    GREEN: "#829E47",
+    GOOGLE_RED: "#E25439",
+    FACEBOOK_BLUE: "#537EB5",
+    RED: "#FF4D4D",
   },
 
   ACTIVE: {
-    GREEN: '#728941',
-    GOOGLE_RED: '#BA4D39',
-    FACEBOOK_BLUE: '#4F719C',
-    RED: '#CC1A1A',
+    GREEN: "#728941",
+    GOOGLE_RED: "#BA4D39",
+    FACEBOOK_BLUE: "#4F719C",
+    RED: "#CC1A1A",
   },
-}
+};
